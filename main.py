e3d18d1b9777a1e7c3314205a95bb8075c8f6d24
aanisar <55286102+aanisar@users.noreply.github.com>
2022-11-21 21:54:59 -0500

    I just fixed the issues with dimensional.

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
 
 model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1662)))
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 126)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
