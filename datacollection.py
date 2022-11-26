966b56ca809c28da12d9fb2d6c7c705008bb686e
aanisar <55286102+aanisar@users.noreply.github.com>
2022-11-26 18:04:12 -0500

    just finsihed collecting the data

--- datacollection.py
+++ datacollection.py
@@ -1,21 +1,23 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+# from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
 import time
-import os
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+# classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
 
 offset = 20
 imgSize = 300
 
-folder = "Data/C"
+folder = "Data/"
 counter = 0
 
 while True:
     success, img = cap.read()
+    # imgOutput = img.copy()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
@@ -35,6 +37,8 @@ while True:
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
+            # prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            # print(prediction, index)
 
         else:
             k = imgSize / w
@@ -43,13 +47,23 @@ while True:
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
             imgWhite[hGap:hCal + hGap, :] = imgResize
+            # prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        # cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
+        #               (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+        # cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        # cv2.rectangle(imgOutput, (x - offset, y - offset),
+        #               (x + w + offset, y + h + offset), (255, 0, 255), 4)
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
+
+
     if key == ord("s"):
         counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
         print(counter)
+s
